17eb926172c9d6fa8d3d6f6930eb8a404c2e966a
Abbie2002 <89632236+Abbie2002@users.noreply.github.com>
2025-02-22 21:17:55 +0530

    Update AdminRequests.js

--- client/src/api/AdminRequests.js
+++ client/src/api/AdminRequests.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://socialmediaapp-backend-nbbq.onrender.com" });
 
 export const fetchAllUsers = () => API.get("/admin/users");
 export const deleteUser = (userId) => API.delete(`/admin/users/${userId}`);
