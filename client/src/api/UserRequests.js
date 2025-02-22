55083339419de8b3ec6d16d7ebe7bdaa3e02da91
Abbie2002 <89632236+Abbie2002@users.noreply.github.com>
2025-02-22 21:20:05 +0530

    Update UserRequests.js

--- client/src/api/UserRequests.js
+++ client/src/api/UserRequests.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://socialmediaapp-backend-nbbq.onrender.com" });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
